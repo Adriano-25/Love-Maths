bb0bff48e61536a8a8bb2ec00971648d806c4c46
Adriano Tolo <adrianotolo24@gmail.com>
2023-03-06 16:44:53 +0000

    html and css done and add function to js

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function increamentScore(){
+
+}
+
+function increamentWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubstractQuestion(){
+
+}
+
+function displayMultipleQuestion(){
+    
+}
